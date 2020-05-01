70cff48a9d36b4ec17c791b64312ae0d4c8cbd5e
Harry Merritt <harry@merritt.es>
2020-05-01 22:53:58 +0100

    Setup sqlite3 db

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,20 @@
 module.exports = {
-    development: {
-        client: "sqlite3",
-        useNullAsDefault: true, // needed for sqlite
-        connection: {
-            filename: "./data/album_covers.db",
-        },
-        migrations: {
-            directory: "./data/migrations",
-        },
-        seeds: {
-            directory: "./data/seeds",
-        },
-        // add the following
-        pool: {
-            afterCreate: (conn, done) => {
-                // runs after a connection is made to the sqlite engine
-                conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
-            },
+    client: "sqlite3",
+    useNullAsDefault: true, // needed for sqlite
+    connection: {
+        filename: "./data/album_covers.db",
+    },
+    migrations: {
+        directory: "./data/migrations",
+    },
+    seeds: {
+        directory: "./data/seeds",
+    },
+    // add the following
+    pool: {
+        afterCreate: (conn, done) => {
+            // runs after a connection is made to the sqlite engine
+            conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
         },
     },
 };
