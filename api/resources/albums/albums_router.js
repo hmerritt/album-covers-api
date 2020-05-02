9bdc0c53cb19e59f461b831f670907c6ae8b3a29
Harry Merritt <harry@merritt.es>
2020-05-02 01:32:56 +0100

    Stream album cover from github

---
+++ api/resources/albums/albums_router.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const axios = require("axios");
+const db = require("./albums_db");
+
+const router = express.Router();
+
+//  Get all albums
+router.get("/", db.validateId(), async (req, res, next) => {
+    res.send(req.album);
+});
+
+//  Get individial album
+router.get("/:id", db.validateId(), async (req, res, next) => {
+    res.send(req.album);
+});
+
+//  Get individial album cover
+router.get("/:id/cover", db.validateId(), async (req, res, next) => {
+    //  Set image header (forces in-line image; no unexpected download)
+    res.set("Content-Type", "image/jpeg");
+
+    //  Github repo base URL
+    const baseUrl =
+        "https://github.com/hmerritt/album-covers/raw/master/covers/";
+
+    //  Full URL to album cover
+    const url =
+        baseUrl +
+        `${req.album.artist}/[${req.album.year}] ${req.album.name}/cover.jpg`;
+
+    //  Fetch album cover from Github
+    //  (creates a stream)
+    const response = await axios({
+        url,
+        method: "GET",
+        responseType: "stream",
+    });
+
+    //  Pipe response stream in to express
+    response.data.pipe(res);
+});
+
+module.exports = router;
