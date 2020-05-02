99006a54fca2b17bfa45cf83e7bb065d48a60bed
Harry Merritt <harry@merritt.es>
2020-05-02 03:06:46 +0100

    Add host ip to express server

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const server = require("./api/server.js");
 
 const PORT = process.env.PORT || 5000;
+const HOST = "0.0.0.0";
 
-server.listen(PORT, () => {
-    console.log(`\n== API running on port ${PORT} ==\n`);
-});
+server.listen(PORT, HOST);
+console.log(`\n== API running on ${HOST}:${PORT} ==\n`);
